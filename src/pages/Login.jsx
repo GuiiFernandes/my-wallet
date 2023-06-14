fb910e8a0d2e4e17a97e1ad1339a0e7465dbe474
Guilherme Fernandes <guifjj92@gmail.com>
2023-06-14 15:59:03 -0300

    feat(Req 1) Criei página de login e configurei seu estado global

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,81 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { saveEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  validInput = () => {
+    const { email, password } = this.state;
+    const MIN_LENGTH = 6;
+    const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+    return emailRegex.test(email) && password.length >= MIN_LENGTH;
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = () => {
+    const { email } = this.state;
+    const { dispatchSaveEmail, history } = this.props;
+    dispatchSaveEmail(email);
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    return (
+      <section>
+        <h1>My Wallet</h1>
+        <form
+          onSubmit={ (e) => {
+            e.preventDefault();
+            this.handleSubmit();
+          } }
+        >
+          <input
+            type="email"
+            data-testid="email-input"
+            name="email"
+            id="email"
+            placeholder="E-mail"
+            onChange={ this.handleChange }
+            required
+          />
+          <input
+            type="password"
+            data-testid="password-input"
+            name="password"
+            id="password"
+            placeholder="Senha"
+            onChange={ this.handleChange }
+            required
+          />
+          <button disabled={ !this.validInput() }>
+            Entrar
+          </button>
+        </form>
+      </section>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatchSaveEmail: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+const mapStateToProps = ({ user: { email } }) => ({ globalEmail: email });
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchSaveEmail: (email) => dispatch(saveEmail(email)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
