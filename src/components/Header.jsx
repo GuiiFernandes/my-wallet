5531786a16c29051386cfa3ce0f3f009ed4df279
Guilherme Fernandes <guifjj92@gmail.com>
2023-06-14 16:24:41 -0300

    feat(Req 2) Criei a estrutura da página de carteira e o header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <header>
+        <div>
+          <h1>My Wallet</h1>
+          <div>
+            <p>
+              <span data-testid="total-field">0</span>
+              {' '}
+              <span data-testid="header-currency-field">BRL</span>
+            </p>
+          </div>
+          <div>
+            <p data-testid="email-field">{email}</p>
+          </div>
+        </div>
+
+      </header>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = ({ user: { email } }) => ({ email });
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import Header from '../components/Header';
+import Table from '../components/Table';
+
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <section>
+        <Header />
+        <Table />
+      </section>
+    );
   }
 }
 
