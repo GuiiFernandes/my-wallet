051013fc374967f2330aa2e4343d5502846682c1
Guilherme Fernandes <guifjj92@gmail.com>
2023-06-15 15:48:28 -0300

    feat(Req 6) Criei tabela de gastos

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 
+import { tableHeaders } from '../helpers';
+
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <table>
+        <thead>
+          <tr>
+            {tableHeaders.map((header) => <th key={ header }>{ header }</th>)}
+          </tr>
+        </thead>
+        <tbody />
+      </table>
     );
   }
 }
